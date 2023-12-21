fb388550a5e3e376fbf15eb59a4da139b95aa7a1
Ramzi Osta <ramziosta@gmail.com>
2023-12-21 13:21:20 -0500

    edits to ,enc env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,9 +6,9 @@ const envSchema = zod.object({
     GOOGLE_CLIENT_SECRET:zod.string().nonempty(),
     NEXTAUTH_URL:zod.string().nonempty(),
     NEXTAUTH_SECRET:zod.string().nonempty(),
-    GITHUB_CLIENT_ID:zod.string().nonempty(),
-    GITHUB_CLIENT_SECRET:zod.string().nonempty(),
-    SECRET:zod.string().nonempty(),
+    // GITHUB_CLIENT_ID:zod.string().nonempty(),
+    // GITHUB_CLIENT_SECRET:zod.string().nonempty(),
+    // SECRET:zod.string().nonempty(),
 });
 
 export const env = envSchema.parse(process.env);
